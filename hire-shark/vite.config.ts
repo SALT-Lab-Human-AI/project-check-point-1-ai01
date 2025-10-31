e2b1083230a613b82f527cd980d07528478efb87
HerobrineXia <HerobrineXia@outlook.com>
2025-10-31 05:38:35 -0500

    Fix local build

--- hire-shark/vite.config.ts
+++ hire-shark/vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/project-check-point-1-ai01/",
   server: {
     host: "::",
     port: 8080,
