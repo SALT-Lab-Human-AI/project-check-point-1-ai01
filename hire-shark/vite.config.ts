db629c16f2f252b405a77a6c8dc8c497aec4b6dc
HerobrineXia <HerobrineXia@outlook.com>
2025-10-31 05:30:57 -0500

    Fix vite config

--- hire-shark/vite.config.ts
+++ hire-shark/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/project-check-point-1-ai01/",
   server: {
     host: "::",
     port: 8080,
