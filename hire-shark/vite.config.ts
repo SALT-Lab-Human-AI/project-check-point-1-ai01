1f9473fe546c701a4b6197a44c5c84b2eeebec2f
HerobrineXia <HerobrineXia@outlook.com>
2025-11-06 22:14:44 -0600

    Update base url for production

--- hire-shark/vite.config.ts
+++ hire-shark/vite.config.ts
@@ -5,26 +5,30 @@ import { componentTagger } from "lovable-tagger";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "./node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
-          dest: "",
-        },
-      ],
-    }),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  return {
+    base: isProduction ? "/project-check-point-1-ai01/" : "/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === "development" && componentTagger(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "./node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
+            dest: "",
+          },
+        ],
+      }),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
